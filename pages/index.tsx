36f7a47b557059bc064b4206ba079ef4531f5090
KucharikCZ <kucharik59.cz@gmail.com>
2024-11-05 15:33:49 +0100

    edit index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,15 @@
 import Cookies from "js-cookie";
 import {useRouter} from "next/router";
+import {useEffect} from "react";
 
 export default function HomePage(){
   const router = useRouter();
 
-  if(Cookies.get("auth_token")){
-    router.push("/dashboard");
-  }else{
-    router.push("/login");
-  }
+  useEffect(() => {
+    if(Cookies.get("auth_token")){
+      router.push("/dashboard");
+    }else{
+      router.push("/login");
+    }
+  },[Cookies,router]);
 }
